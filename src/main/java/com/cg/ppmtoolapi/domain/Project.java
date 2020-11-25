5984956c3a5864f222877b8c60f96c893a86656d
sushmithareddy10 <sushmithareddy1098@gmail.com>
2020-11-25 11:57:34 +0530

    Added comments

--- src/main/java/com/cg/ppmtoolapi/domain/Project.java
+++ src/main/java/com/cg/ppmtoolapi/domain/Project.java
@@ -1,8 +1,8 @@
 package com.cg.ppmtoolapi.domain;
 
 /**
- * This is the domain class that will be representing the project 
- * detail and will be responsible as a data traveler object
+ * This is the domain class that will be representing the project.
+ * detail and will be responsible as a data traveler object.
  * @author SUSHMITHA
  *
  */
