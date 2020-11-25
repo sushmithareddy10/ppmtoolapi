4281e387ae6088a79a112d86fcd067fd169913a1
sushmithareddy10 <sushmithareddy1098@gmail.com>
2020-11-25 11:46:33 +0530

    Initial Commit

---
+++ src/main/java/com/cg/ppmtoolapi/domain/Project.java
@@ -0,0 +1,11 @@
+package com.cg.ppmtoolapi.domain;
+
+/**
+ * This is the domain class that will be representing the project 
+ * detail and will be responsible as a data traveler object
+ * @author SUSHMITHA
+ *
+ */
+public class Project {
+
+}
